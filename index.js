f3c0778473dd55167ad661f92deb239fe08d2409
Elaine Ng <128241985+elainengg@users.noreply.github.com>
2024-12-03 13:30:00 -0500

    a6 pt 3

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
+
 const app = express()
 app.use(
     cors({
