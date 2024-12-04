c80f52dfd76f2e34677c133fc54d0301fc1301ae
Elaine Ng <128241985+elainengg@users.noreply.github.com>
2024-12-03 23:09:27 -0500

    before 5.2.4

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -45,19 +45,20 @@ const findCoursesForEnrolledUser = (req, res) => {
   
   app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
   
-  app.get("/api/courses/:courseId/modules", (req, res) => {
+  app.get("/api/courses/:courseId/modules", async (req, res) => {
     const { courseId } = req.params;
-    const modules = modulesDao.findModulesForCourse(courseId);
+    const modules = await modulesDao.findModulesForCourse(courseId);
     res.json(modules);
   });
+ 
 
-  app.post("/api/courses/:courseId/modules", (req, res) => {
+  app.post("/api/courses/:courseId/modules", async (req, res) => {
     const { courseId } = req.params;
     const module = {
       ...req.body,
       course: courseId,
     };
-    const newModule = modulesDao.createModule(module);
+    const newModule = await modulesDao.createModule(module);
     res.send(newModule);
   });
 
