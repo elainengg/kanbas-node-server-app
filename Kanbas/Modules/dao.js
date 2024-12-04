3e997482df80147c8d62e309dbdb3b04d092bdf2
Elaine Ng <128241985+elainengg@users.noreply.github.com>
2024-12-04 14:15:51 -0500

    before 5.3

--- Kanbas/Modules/dao.js
+++ Kanbas/Modules/dao.js
@@ -21,3 +21,12 @@ export function createModule(module) {
   // Database.modules = modules.filter((module) => module._id !== moduleId);
  }
  
+// i dont know when i got this
+ export function updateModule(moduleId, moduleUpdates) {
+  return model.updateOne({ _id: moduleId }, moduleUpdates);
+  // const { modules } = Database;
+  // const module = modules.find((module) => module._id === moduleId);
+  // Object.assign(module, moduleUpdates);
+  // return module;
+ }
+ 
